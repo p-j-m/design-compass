c1b7fd47a028c5c737886e5de64d6aa9e5e488db
bckdmnd <garadiansss@gmail.com>
2017-11-04 11:11:45 +0900

    dd

--- js/index.js
+++ js/index.js
@@ -1,21 +1,24 @@
-$(documnet).load(function () {
-    $(".se-pre-con").fadeOut("slow");
-});
-$(document).ready(function () {
+
+$(document).ready(function() {
 
     $('.animate').scrolla({
-        mobile: true,
-        once: true
-    });
+		mobile: true,
+		once: true
+	});
 
-    $('#button-show-window').click(function () {
-        var template = $('#modal-template').html();
-        $('body').append(template);
-        $('.animate').scrolla('bind');
-        $('section.modal .modal-close').click(function () {
-            $(this).closest('section.modal').remove();
-        });
+	$('#button-show-window').click(function() {
+		var template = $('#modal-template').html();
+		$('body').append(template);
+		$('.animate').scrolla('bind');
+		$('section.modal .modal-close').click(function() {
+			$(this).closest('section.modal').remove();
+		});
 
-        return false;
-    });
+		return false;
+	});
 });
+
+$(documnet).load(function() {
+		// Animate loader off screen
+		$(".se-pre-con").fadeOut("slow");;
+	});
