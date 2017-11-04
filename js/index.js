54c72d170804fb9415ad9c6a23301d8066ccb4ba
bckdmnd <garadiansss@gmail.com>
2017-11-04 11:00:39 +0900

    디자인 책장 v1
    업로드!

---
+++ js/index.js
@@ -0,0 +1,25 @@
+
+	$(window).load(function() {
+		// Animate loader off screen
+		$(".se-pre-con").fadeOut("slow");;
+	});
+
+
+$(document).ready(function() {
+
+    $('.animate').scrolla({
+		mobile: true,
+		once: true
+	});
+
+	$('#button-show-window').click(function() {
+		var template = $('#modal-template').html();
+		$('body').append(template);
+		$('.animate').scrolla('bind');
+		$('section.modal .modal-close').click(function() {
+			$(this).closest('section.modal').remove();
+		});
+
+		return false;
+	});
+});
