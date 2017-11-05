7852e13a360fff6cebb7c61a1bb470f7e02fe082
bckdmnd <garadiansss@gmail.com>
2017-11-05 20:27:41 +0900

    파일 정리

--- js/index.js
+++ js/index.js
@@ -14,53 +14,11 @@ $(document).ready(function() {
 		});
 
 		return false;
-	});
-    
-    
-    
+	}); 
     
 });
 
 $(window).load(function() {
-		// Animate loader off screen
 		$(".se-pre-con").fadeOut("slow");
-    
-$(function(){
-  var getMatchingUri = function(){
-    this.init();
-  };
-
-  getMatchingUri.prototype = {
-    init : function(){
-      this.cacheElement();
-      this.setEvent();
-    },
-    cacheElement : function(){
-      this.wel = $("#gnb");
-      this.el = this.wel.find("a");
-    },
-    click : function(e){
-      var uri = e.currentTarget.href,
-          pattern = /\#(?:.*)/g,
-          pageString = uri.match(pattern)[0];
-
-      $.each(this.el, function(){
-        var $this = $(this);
-        if(pageString === $this.attr("href")){
-          $this.parent().addClass("current");
-        } else {
-          $this.parent().removeClass("current");
-        }
-
-      });
-    },
-    setEvent : function(){
-      this.el.on("click", $.proxy(this.click, this));
-    }
-  };
-
-  var getmatchuri = new getMatchingUri();
-}());
-    
-	});
+});
 
