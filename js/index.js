62a7a690aa20fe2b39d745e5fc80eed5928995b1
bckdmnd <garadiansss@gmail.com>
2017-11-30 20:02:52 +0900

    페이지네이션 업데이트

--- js/index.js
+++ js/index.js
@@ -1,27 +1,68 @@
-
-$(window).load(function() {
-		$(".se-pre-con").fadeOut(800);
+$(window).load(function () {
+    $(".se-pre-con").fadeOut(800);
 });
 
-$(document).ready(function() {
-      $('.animate').scrolla({
-		mobile: true,
-		once: true
-	});
-
-	$('#button-show-window').click(function() {
-		var template = $('#modal-template').html();
-		$('body').append(template);
-		$('.animate').scrolla('bind');
-		$('section.modal .modal-close').click(function() {
-			$(this).closest('section.modal').remove();
-		});
-
-		return false;
-	}); 
-
+$(document).ready(function () {
     
+            // Hide Header on on scroll down
+            var didScroll;
+            var lastScrollTop = 0;
+            var delta = 5;
+            var navbarHeight = $('header').outerHeight();
+            $(window).scroll(function(event) {
+                didScroll = true;
+            });
+            setInterval(function() {
+                if (didScroll) {
+                    hasScrolled();
+                    didScroll = false;
+                }﻿
+            }, 300);
+
+            function hasScrolled() {
+                var st = $(this).scrollTop();
+
+                // Make sure they scroll more than delta
+                if (Math.abs(lastScrollTop - st) <= delta)
+                    return;
+
+                // If they scrolled down and are past the navbar, add class .nav-up.
+                // This is necessary so you never see what is "behind" the navbar.
+                if (st > lastScrollTop && st > navbarHeight) {
+                    // Scroll Down
+                    $('header').removeClass('nav-down').addClass('nav-up');
+                } else {
+                    // Scroll Up
+                    if (st + $(window).height() < $(document).height()) {
+                        $('header').removeClass('nav-up').addClass('nav-down');
+                    }
+                }
+
+                lastScrollTop = st;
+            }
+   
+    
+    $('.animate').scrolla({
+        mobile: true,
+        once: true
+    });
+
+    $('#button-show-window').click(function () {
+        var template = $('#modal-template').html();
+        $('body').append(template);
+        $('.animate').scrolla('bind');
+        $('section.modal .modal-close').click(function () {
+            $(this).closest('section.modal').remove();
+        });
+
+        return false;
+    });
+
+
+
+
 });
 
-       
+
+
 
